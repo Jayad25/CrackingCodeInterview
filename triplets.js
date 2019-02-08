7da4f6fe253476bc073618ad45dff823d931d374
jayad25 <dammalapati.jaya@gmail.com>
2019-02-07 16:24:15 -0800

    thressum with updated time complexity

--- triplets.js
+++ triplets.js
@@ -12,4 +12,28 @@ function triplets(array,target) {
     return result
 }
 
+
+function triplets_time(array, target) {
+    let result = []
+    let sorted_array = array.sort((a, b) => a - b)
+    for (let i = 0; i < sorted_array.length - 2; i++) {
+      let left = i + 1;
+      let right = sorted_array.length - 1
+      while (left < right){
+        let current_sum = sorted_array[i] + sorted_array[left] + sorted_array[right]
+        if (current_sum === target){
+            result.push([sorted_array[i],sorted_array[left],sorted_array[right]])
+            left++;
+            right--;
+        }else if(current_sum < target){
+            left++
+        }else{
+            right--
+        }
+      }
+    }
+    return result
+}
+
 console.log(triplets([12,3,1,2,-6,5,-8,6],0))
+console.log(triplets_time([12,3,1,2,-6,5,-8,6],0))
