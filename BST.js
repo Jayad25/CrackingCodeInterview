3feb22158704d11adef8ef658e3282b06496fd86
jayad25 <dammalapati.jaya@gmail.com>
2019-02-16 14:00:51 -0800

    remove element from BST

--- BST.js
+++ BST.js
@@ -64,10 +64,47 @@ class BST {
         return false
     }
 
-    remove(value) {
+    remove(value,parent=null) {
         // Write your code here.
         // Do not edit the return statement of this method.
+      if(value < this.value){
+          if(this.left !== null){
+              this.left.remove(value,this)
+          }
+      }else if(value > this.value){
+          if(this.right !== null){
+              this.right.remove(value,this)
+          }
+      }else{
+          if(this.left !== null && this.right !== null){
+              this.value = this.right.getMinvalue()
+                this.right.remove(this.value,this);
+          }else if(parent === null){
+              if(this.left !== null){
+                  this.value = this.left.value;
+                  this.right = this.left.right;
+                  this.left = this.left.left;
+              }else if(this.right !== null){
+                  this.value = this.right.value;
+                  this.right = this.right.right;
+                  this.left = this.right.left;
+              }else{
+                  this.value = null;
+              }
+          }else if(parent.left === this){
+            parent.left = this.left !== null ? this.left : this.right;
+          }else if (parent.right === this){
+              parent.right = this.left !== null ? this.left : this.right;
+          }
+      }
         return this;
+    }
+    getMinvalue(){
+        if(this.left === null){
+            return this.value
+        }else{
+            return this.left.getMinvalue();
+        }
     }
 }
 
