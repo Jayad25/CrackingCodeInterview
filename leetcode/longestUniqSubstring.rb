7e2241af98a7bad295076620881be1f3e2718b1e
jayad25 <dammalapati.jaya@gmail.com>
2019-02-21 15:48:55 -0800

    update substring with inner values

--- leetcode/longestUniqSubstring.rb
+++ leetcode/longestUniqSubstring.rb
@@ -7,12 +7,16 @@ end
 
 def sub_string(s)
     sub_strings = []
-    (0..s.length-1).each do |i|
-        (i).upto(s.length-1) do |j|
-            sub_strings.push(s[i,j]) unless sub_strings.include?(s[i,j])
+
+    (0...s.length-1).each do |i|
+       
+        (i).upto(s.length) do |j|
+             p i, j
+             p  s[i..j]
+            sub_strings.push(s[i..j]) 
         end
     end
-    sub_strings.push(s).sort_by {|ch| ch.length}
+    sub_strings.sort_by {|ch| ch.length}
 end
-p sub_string("bbbbb")
-p length_of_longest_substring("pwwkew")
+p sub_string("cdd")
+p length_of_longest_substring("cdd")
