b1a21941a1112714fad7be15a9b6fe2d1825fad2
jayad25 <dammalapati.jaya@gmail.com>
2019-04-03 09:49:06 -0500

    find pivot index for rotated array

--- searchInRotatedSortedArray.js
+++ searchInRotatedSortedArray.js
@@ -1,5 +1,11 @@
 
 function search(nums,target){
+let pivotIndex = pivot(0,nums.length,nums)
+    if(nums[pivotIndex]<target && nums[nums.length - 1] > target){
+        return pivotIndex+ bSearch(nums.splice(pivotIndex),target)
+    }else{
+        return bSearch(nums.splice(0,pivotIndex-1),target)
+    }
 
 }
 function pivot(s,e,nums){
@@ -10,11 +16,36 @@ function pivot(s,e,nums){
         return m+1
     }
     else if(nums[s] > nums[m]){
-         pivot(s,m-1,nums)
+         return pivot(s,m-1,nums)
     }else{
-         pivot(m+1,e,nums)
+         return pivot(m+1,e,nums)
     }
 }
-nums = [2,3,4,5,1]
+function bSearch(nums,target){
+    if(nums.length === 0){
+        return -1
+    }
+    mid = Math.floor(nums.length/2)
+    if(nums[mid] === target){
+        return mid+1;
+    }else if (nums[mid] > target){
+        return bSearch(nums.splice(0,mid),target)
+    }else{
+        x = mid;
+        res=  bSearch(nums.splice(mid+1),target)
+        // console.log(mid)
+         return res == -1 ? -1 : res+1+x
+    }
+}
 
-console.log(pivot(0,nums.length,nums))
+
+
+
+
+
+
+nums = [3,4,5,9,1,2]
+
+// console.log(pivot(0,nums.length,nums))
+// console.log(bSearch(nums,78))
+console.log(search(nums,1))
